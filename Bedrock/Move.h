3ee41d3857af6c423d42444756b4e8a1acf92009
Jeremy <jeremy.laumon@gmail.com>
2024-11-15 11:01:38 +0100

    Force-inlined gMove and gForward

--- Bedrock/Move.h
+++ Bedrock/Move.h
@@ -4,19 +4,31 @@
 #include <Bedrock/Core.h>
 #include <Bedrock/TypeTraits.h>
 
+// We want gMove/gForward to be always inlined, even when optimizations are disabled.
+// __forceinline doesn't work in debug in MSVC, but [[msvc::intrinsic]] does (it only works in very specific conditions though).
+#ifdef __clang__
+#define ATTRIBUTE_INTRINSIC __forceinline
+#elif _MSC_VER
+#define ATTRIBUTE_INTRINSIC [[msvc::intrinsic]]
+#else
+#define ATTRIBUTE_INTRINSIC
+#endif
+
 
 // Equivalent to std::move
 template <typename taType>
-[[nodiscard]] constexpr RemoveReference<taType>&& gMove(taType&& ioArg) { return static_cast<RemoveReference<taType>&&>(ioArg); }
+[[nodiscard]] ATTRIBUTE_INTRINSIC constexpr RemoveReference<taType>&& gMove(taType&& ioArg) { return static_cast<RemoveReference<taType>&&>(ioArg); }
 
 
 /// Equivalent to std::forward
 template <typename taType>
-[[nodiscard]] constexpr taType&& gForward(RemoveReference<taType>& ioArg) { return static_cast<taType&&>(ioArg); }
+[[nodiscard]] ATTRIBUTE_INTRINSIC constexpr taType&& gForward(RemoveReference<taType>& ioArg) { return static_cast<taType&&>(ioArg); }
 template <typename taType>
-[[nodiscard]] constexpr taType&& gForward(RemoveReference<taType>&& ioArg)
+[[nodiscard]] ATTRIBUTE_INTRINSIC constexpr taType&& gForward(RemoveReference<taType>&& ioArg)
 {
 	static_assert(!cIsLValueReference<taType>, "Can't forward an lvalue reference.");
 	return static_cast<taType&&>(ioArg);
 }
 
+
+#undef ATTRIBUTE_INTRINSIC
