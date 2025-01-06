33bc8890b7b63518b5d8897098ac4a0e1ac12cca
Jeremy <jeremy.laumon@gmail.com>
2025-01-06 20:35:07 +0100

    Added Array

---
+++ Bedrock/Array.cpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MPL-2.0
+#include <Bedrock/Array.h>
+#include <Bedrock/Span.h>
+#include <Bedrock/Test.h>
+
+REGISTER_TEST("Array")
+{
+	Array<int, 5> values = { 1, 2, 3, 4, 5 };
+	Span test = values;
+
+	// Mostly checking that things compile.
+	TEST_TRUE(test == values);
+};
---
+++ Bedrock/Array.h
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MPL-2.0
+#pragma once
+
+#include <Bedrock/Core.h>
+#include <Bedrock/TypeTraits.h>
+
+template <typename taType, int taSize>
+struct Array
+{
+	using ValueType = taType;
+
+	constexpr static int Size() { return taSize; }
+
+	constexpr const taType* Data() const { return mData; }
+	constexpr taType* Data() { return mData; }
+
+	constexpr const taType* Begin() const { return mData; }
+	constexpr const taType* End() const { return mData + taSize; }
+	constexpr const taType* begin() const { return mData; }
+	constexpr const taType* end() const { return mData + taSize; }
+	constexpr taType* Begin() { return mData; }
+	constexpr taType* End() { return mData + taSize; }
+	constexpr taType* begin() { return mData; }
+	constexpr taType* end() { return mData + taSize; }
+	
+	constexpr const taType& operator[](int inPosition) const { gBoundsCheck(inPosition, taSize); return mData[inPosition]; }
+	constexpr taType& operator[](int inPosition) { gBoundsCheck(inPosition, taSize); return mData[inPosition]; }
+
+	taType mData[taSize];
+};
+
+
+// Array is a contiguous container.
+template<class taType, int taSize> inline constexpr bool cIsContiguous<Array<taType, taSize>> = true;
