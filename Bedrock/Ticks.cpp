51f8c86f7aab622f0d3569f928b93276b17e840e
Jeremy <jeremy.laumon@gmail.com>
2025-03-16 13:19:50 +0100

    Fixed int64 overflow when converting large number of ticks to nanoseconds

--- Bedrock/Ticks.cpp
+++ Bedrock/Ticks.cpp
@@ -7,23 +7,24 @@
 #include <Windows.h>
 
 
-int64 sGetTicksPerSecond()
+// Note: QPC resolution is 100ns, but if we ever need more precision we could use tenths of nanosecond here instead.
+static int64 sGetNanosecondsPerTick()
 {
-	static const int64 sTicksPerSecond = []
+	static const int64 sNanosecondsPerTick = []
 	{
-		LARGE_INTEGER frequency;
+		LARGE_INTEGER frequency { .QuadPart = 1 };
 		QueryPerformanceFrequency(&frequency);
 
-		return frequency.QuadPart;
+		return 1'000'000'000 / frequency.QuadPart;
 	}();
 
-	return sTicksPerSecond;
+	return sNanosecondsPerTick;
 }
 
 
 int64 gGetTickCount()
 {
-	LARGE_INTEGER counter;
+	LARGE_INTEGER counter = {};
 	QueryPerformanceCounter(&counter);
 	return counter.QuadPart;
 }
@@ -31,7 +32,7 @@ int64 gGetTickCount()
 
 int64 gTicksToNanoseconds(int64 inTicks)
 {
-	return inTicks * 1'000'000'000 / sGetTicksPerSecond();
+	return inTicks * sGetNanosecondsPerTick();
 }
 
 
@@ -51,7 +52,7 @@ double gTicksToSeconds(int64 inTicks)
 
 int64 gNanosecondsToTicks(int64 inNanoseconds)
 {
-	return inNanoseconds * sGetTicksPerSecond() / 1'000'000'000;
+	return inNanoseconds / sGetNanosecondsPerTick();
 }
 
 
