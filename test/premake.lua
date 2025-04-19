67dc80d1d5fe8a12fce5b47eb0301699d1a7fbcb
Jeremy <jeremy.laumon@gmail.com>
2025-04-19 20:34:58 +0200

    Added Clang configs in premake
    Also fixed Release.
    Clang + ASAN does not work, unclear why.

--- test/premake.lua
+++ test/premake.lua
@@ -1,6 +1,6 @@
 solution "BedrockTest"
 	
-	platforms { "x64" }
+	platforms { "x64", "Clang" }
 	configurations { "Debug", "DebugASAN", "DebugOpt", "Release" }
 	startproject "BedrockTest"
 
@@ -11,37 +11,55 @@ solution "BedrockTest"
 		cppdialect "C++latest"
 		exceptionhandling "Off"
 		rtti "Off"
+		architecture "x64"
 		flags 
 		{
 			"MultiProcessorCompile",
 			"FatalWarnings"
 		}
+
+		buildoptions
+		{
+			"/utf-8" 
+		}
 		
-		filter { "toolset:msc*" }
-			buildoptions
-			{
-				"/utf-8" 
-			}
+		filter { "platforms:Clang" }
+			toolset "clang"
 
 		filter { "configurations:Debug" }
 			targetsuffix "Debug"
 			defines "ASSERTS_ENABLED"
 			optimize "Debug"
+			runtime "Debug"
 			editandcontinue "On"
 			
 		filter { "configurations:DebugASAN" }
 			targetsuffix "DebugASAN"
 			defines "ASSERTS_ENABLED"
 			optimize "Debug"
+			runtime "Debug"
 			editandcontinue "Off"	  -- incompatible with ASAN
 			flags "NoIncrementalLink" -- incompatible with ASAN
 			sanitize "Address"
-			
+
+		-- Note: ASAN + Clang is not working. This is enough to make it link, but it doesn't run.
+		-- filter { "configurations:DebugASAN", "platforms:Clang" }
+		-- 	runtime "Release"		  -- debug runtime is incompatible with ASAN (clang-cl)
+		-- 	links 
+		-- 	{
+		-- 		-- apparently VS doesn't automatically link the libs when using ASAN with clang-cl
+		-- 		"clang_rt.asan_dynamic-x86_64",
+		-- 		"clang_rt.asan_dynamic_runtime_thunk-x86_64"
+		-- 	}
+
 		filter { "configurations:DebugOpt" }
 			targetsuffix "DebugOpt"
 			defines "ASSERTS_ENABLED"
 			optimize "Full"
 			editandcontinue "On"
+
+		filter { "configurations:Release" }
+			optimize "Full"
 			
 		filter {}
 
